d74312f11eecaff7e0e45cd1619dd81ca5b675ce
Yuri Okolnov <yuokol@yandex.ru>
2021-11-19 17:40:07 +0300

    filling list and drawing pins

--- Board/Board.cs
+++ Board/Board.cs
@@ -4,6 +4,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
 
 namespace PinBoard
 {
@@ -15,6 +17,21 @@ namespace PinBoard
         }
 
         public List<Pin> Pins;
+
+        /// <summary>
+        /// Add Pin
+        /// </summary>
+        /// <param name="px">Pin x</param>
+        /// <param name="py">Pin y</param>
+        /// <param name="names">Joined names of component and pin</param>
+        public void AddPin(float px, float py, string names)
+        {
+            string[] namesplit  = names.Split('_');
+            // After N_PIN there may be some text that can't be split like this.
+            // But I dont know what you want to do if its like this.
+            // So I'm not adding any specific processing of that error here.
+            Pins.Add(new Pin {X = px, Y = py, ComponentName = namesplit[0], Name = namesplit[1]});
+        }
 
         /// <summary>
         /// Calculate the centre of gravity of all the points on the board.
@@ -53,6 +70,68 @@ namespace PinBoard
                 pin.X = (float)(xTurn + xRel * Math.Cos(rad) - yRel * Math.Sin(rad));
                 pin.Y = (float)(yTurn + xRel * Math.Sin(rad) + yRel * Math.Cos(rad));
             }
+        }
+
+        /// <summary>
+        /// Read pins from file and return any misformed data as string
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public string ReadPinsFromFile(string path)
+        {
+            Pins = new List<Pin>();
+
+            string readPath = path;
+            //@"C:\Users\YuOko\OneDrive\Desktop\Work\ROBATEST\ТЗ_Robat1.txt";
+            string returnText = "";
+
+            using (StreamReader sr = new StreamReader(readPath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string fileLine = sr.ReadLine();
+                    string pinDesc = fileLine.Replace("N_PIN ", "");
+                    if(fileLine.Length > pinDesc.Length)
+                    {
+                        string[] pinData = pinDesc.Split(' ');
+
+                        //  ignoring any unconventional pin descriptions
+                        if (pinData.Length == 3)
+                        {
+                            string names = pinData[0];
+                            string px = pinData[1];
+                            string py = pinData[2];
+                            
+                            try
+                            {
+                                // adding pin
+                                float pxf, pyf;
+                                if (TryParseGBFloat(px, out pxf) && TryParseGBFloat(py, out pyf))
+                                    AddPin(pxf, pyf, names);
+                                else
+                                    //  catching any unparseable pin descriptions
+                                    returnText += pinDesc + "\n";
+                            }
+                            catch
+                            {
+                                //  catching any problematic pin descriptions
+                                returnText += pinDesc + "\n";
+                            }
+                        }
+                        else 
+                        {
+                            returnText += pinDesc + "\n";
+                        }
+
+                    }
+                }
+            }
+            return returnText;
+        }
+
+        public static bool TryParseGBFloat(string txt, out float number)
+        {
+            return float.TryParse(txt, NumberStyles.Float, CultureInfo.GetCultureInfo("en-GB"), out number);
         }
     }
 }
--- PinDataAnalyzer/MainWindow.xaml.cs
+++ PinDataAnalyzer/MainWindow.xaml.cs
@@ -14,6 +14,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 using System.IO;
+using PinBoard;
+using Microsoft.Win32;
+using System.Globalization;
 
 namespace PinDataAnalyzer
 {
@@ -22,9 +25,11 @@ namespace PinDataAnalyzer
     /// </summary>
     public partial class MainWindow : Window
     {
+        Board board;
         public MainWindow()
         {
             InitializeComponent();
+            board = new Board();
         }
 
         private void buttonProcess_Click(object sender, RoutedEventArgs e)
@@ -33,8 +38,7 @@ namespace PinDataAnalyzer
             DateTime start = DateTime.Now;
             string text = "";
             using (StreamReader sr = new StreamReader(readPath))
-            {
-                
+            {                
                 while (!sr.EndOfStream)
                 {
                     string fileLine = sr.ReadLine();
@@ -43,6 +47,107 @@ namespace PinDataAnalyzer
             }
             DateTime end = DateTime.Now;
             MessageBox.Show(string.Format("start {0}, end {1}", start, end));
+        }
+
+        /// <summary>
+        /// open file, read pins from it and fill listbox and board
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonChooseInputFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if ((bool)ofd.ShowDialog())
+            {
+                // read pins to board object
+                string path = ofd.FileName;
+                string problems = board.ReadPinsFromFile(path);
+                if(problems != string.Empty)
+                    MessageBox.Show("While reading pins, some lines were misformed:\n" + problems);
+
+                // write components to listbox
+                var components = board.Pins.GroupBy(pin => pin.ComponentName)
+                    .Select( group => new
+                    {
+                        component = group.Key,
+                        pins = group.Count()
+                    }
+                    ).OrderBy(grp => grp.component).ToList();
+
+                lbComponents.ItemsSource = components;
+
+                // draw pins to board canvas
+                DrawBoard();
+            }
+        }
+
+        /// <summary>
+        /// draw pins on board canvas
+        /// </summary>
+        void DrawBoard()
+        {
+            //cBoard.
+            // draw pins to board canvas
+            foreach (Pin pin in board.Pins)
+            {
+                DrawPin((int)pin.X, (int)pin.Y);
+            }
+        }
+
+        /// <summary>
+        /// drawing circle for pin
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        void DrawPin(int x, int y)
+        {
+            int radius = 1;
+
+            Point point = new Point(x, y);
+            Ellipse ellipse = new Ellipse();
+
+            ellipse.Width = radius * 2;
+            ellipse.Height = radius * 2;
+
+            ellipse.StrokeThickness = 1;
+            ellipse.Stroke = Brushes.Blue;
+            ellipse.Margin = new Thickness(point.X - radius, point.Y - radius, 0, 0);
+            cBoard.Children.Add(ellipse);
+        }
+
+        /// <summary>
+        /// rotate board with pins
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bRotate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                float degree, centerX, centerY;
+                if (!Board.TryParseGBFloat(tbDegree.Text, out degree))
+                {
+                    MessageBox.Show("Bad format of degree");
+                    return;
+                }
+                if (!Board.TryParseGBFloat(tbAroundX.Text, out centerX))
+                {
+                    MessageBox.Show("Bad format of pivot x");
+                    return;
+                }                
+                if (!Board.TryParseGBFloat(tbAroundY.Text, out centerY))
+                {
+                    MessageBox.Show("Bad format of pivot y");
+                    return;
+                }
+                board.Turn(degree, centerX, centerY);
+
+                DrawBoard();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
